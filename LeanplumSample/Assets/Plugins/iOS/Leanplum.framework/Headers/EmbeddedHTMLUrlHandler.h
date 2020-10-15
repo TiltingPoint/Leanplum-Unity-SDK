4da3b3b00ab0cd8fe69d738066f0dd9e2fc28cf5
Santiago Castaneda Munoz <santiagocm98@hotmail.com>
2020-10-14 19:56:03 -0500

    Updating Leanplum Sample With Latest Libs

---
+++ LeanplumSample/Assets/Plugins/iOS/Leanplum.framework/Headers/EmbeddedHTMLUrlHandler.h
@@ -0,0 +1,13 @@
+//
+//  EmbeddedHTMLUrlHandler.h
+//  LeanplumSDK-iOS
+//
+//  Created by Santiago Castañeda Muñoz on 10/14/20.
+//  Copyright © 2020 Leanplum. All rights reserved.
+//
+
+#ifndef EmbeddedHTMLUrlHandler_h
+#define EmbeddedHTMLUrlHandler_h
+
+
+#endif /* EmbeddedHTMLUrlHandler_h */
